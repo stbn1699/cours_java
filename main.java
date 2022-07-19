160e675e1ae93e44725afa03d6f209994e94d4c4
ebasson <esteban.basson@pwa-assurance.fr>
2022-07-19 11:11:47 +0200

    finish

--- main.java
+++ main.java
@@ -4,16 +4,40 @@ class HelloWorld {
     public static void main(String[] args) {
 
         Scanner saisieUtilisateur = new Scanner(System.in);
-        System.out.print("\nVeuillez saisir le nombre a sommer : ");
-        int nombreSommer = saisieUtilisateur.nextInt();
-        System.out.println(" ");
-        int somme = 0;
 
-        for(int i = 1; i != nombreSommer + 1 ; i++){
-            somme = somme + i;
+        int nombreAleatoire = (int)(Math.random() * 100);
+        int nEssais = 10;
+
+        System.out.println("\nnombre aléatoire généré!");
+
+        boolean trouve = false;
+        boolean perdu = false;
+
+        while(!trouve && !perdu){
+            System.out.print("il vous reste " + nEssais + " essais, veuillez entrer un nombre : ");
+            int guess = saisieUtilisateur.nextInt();
+
+            if(guess == nombreAleatoire){
+                trouve = true;
+            } else if (guess < nombreAleatoire) {
+                System.out.println("\tvotre nombre est plus petit\n");
+            } else if (guess > nombreAleatoire) {
+                System.out.println("\tvotre nombre est plus grand\n");
+            }
+
+            if(nEssais == 0){
+                perdu = true;
+            }
+
+            nEssais--;
+
         }
 
-        System.out.println("\tla somme de " + nombreSommer + " est égale à : " + somme);
+        if(trouve){
+            System.out.print("\n\tVous avez trouvé " + nombreAleatoire + " en " + (10 - nEssais) + " essais\n");
+        }else if (perdu){
+            System.out.print("\tVous avez perdu, le nombre était " + nombreAleatoire + "\n");
+        }
 
     }
 }
