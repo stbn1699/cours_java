f6223da48cd7e1a66e0fe77a8462ec4af637d9e4
ebasson <esteban.basson@pwa-assurance.fr>
2022-07-19 15:31:13 +0200

    commentaires

--- main.java
+++ main.java
@@ -4,15 +4,19 @@ class HelloWorld {
     public static void main(String[] args) {
 
         Scanner saisieUtilisateur = new Scanner(System.in);
+
+        /* demande a l'utilisateur le nombre qu'il veux sommer */
         System.out.print("\nVeuillez saisir le nombre a sommer : ");
         int nombreSommer = saisieUtilisateur.nextInt();
         System.out.println(" ");
         int somme = 0;
 
+        /* calcul de la somme du nombre d de l'utilisateur */
         for(int i = 1; i != nombreSommer + 1 ; i++){
             somme = somme + i;
         }
 
+        /* affichage du résultat */
         System.out.println("\tla somme de " + nombreSommer + " est égale à : " + somme);
 
     }
