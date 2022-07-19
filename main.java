1039c89b11b5cb4b9f60ea442d9e7471ffeb8e08
ebasson <esteban.basson@pwa-assurance.fr>
2022-07-19 15:26:51 +0200

    commentaires

--- main.java
+++ main.java
@@ -1,9 +1,11 @@
 class HelloWorld {
     public static void main(String[] args) {
 
+        /* création et affichage du mot avant retournement */
         String exemple = "bonjour";
         System.out.println(exemple);
 
+        /* retournement et affichage du mot                */
         StringBuilder Reverse = new StringBuilder(exemple);
         exemple = Reverse.reverse().toString();
         System.out.println(exemple);
