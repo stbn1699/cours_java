fd6272be1910e746ac43834107b9bfb65f7fad4b
ebasson <esteban.basson@pwa-assurance.fr>
2022-07-19 15:34:08 +0200

    commentaires

--- main.java
+++ main.java
@@ -5,6 +5,7 @@ class HelloWorld {
 
         Scanner saisieUtilisateur = new Scanner(System.in);
 
+        /* génération du nombre aléatoire */
         int nombreAleatoire = (int)(Math.random() * 100);
         int nEssais = 10;
 
@@ -14,9 +15,12 @@ class HelloWorld {
         boolean perdu = false;
 
         while(!trouve && !perdu){
+
+            /* demande a l'utilisateur d'entrer un nombre */
             System.out.print("il vous reste " + nEssais + " essais, veuillez entrer un nombre : ");
             int guess = saisieUtilisateur.nextInt();
 
+            /* regarde si le nombre est juste, trop haut ou trop bas */
             if(guess == nombreAleatoire){
                 trouve = true;
             } else if (guess < nombreAleatoire) {
@@ -25,6 +29,7 @@ class HelloWorld {
                 System.out.println("\tvotre nombre est plus grand\n");
             }
 
+            /* regarde s'il reste des essais a l'utilisateur */
             if(nEssais == 0){
                 perdu = true;
             }
@@ -33,6 +38,7 @@ class HelloWorld {
 
         }
 
+        /* affiche la fin en fonction de si l'utilisateur a trouvé ou q'il n'a plus d'essais */
         if(trouve){
             System.out.print("\n\tVous avez trouvé " + nombreAleatoire + " en " + (10 - nEssais) + " essais\n");
         }else if (perdu){
