ea19471566009cb4d2f012d1d390a514a0654bd7
ebasson <esteban.basson@pwa-assurance.fr>
2022-07-19 10:04:33 +0200

    finish

--- main.java
+++ main.java
@@ -4,20 +4,16 @@ class HelloWorld {
     public static void main(String[] args) {
 
         Scanner saisieUtilisateur = new Scanner(System.in);
-
-        System.out.print("Veuillez saisir la taille de la base de la pyramide : ");
-        int base = saisieUtilisateur.nextInt();
+        System.out.print("\nVeuillez saisir le nombre a sommer : ");
+        int nombreSommer = saisieUtilisateur.nextInt();
         System.out.println(" ");
+        int somme = 0;
 
-        int taille = 1;
-        while(base != 0){
-            for(int i = 0; i < taille; i++){
-                System.out.print("*");
-            }
-            System.out.println(" ");
-            taille++;
-            base--;
+        for(int i = 1; i != nombreSommer + 1 ; i++){
+            somme = somme + i;
         }
+
+        System.out.println("\tla somme de " + nombreSommer + " est égale à : " + somme);
 
     }
 }
